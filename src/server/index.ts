59a88b0a36992c2f0b4c17ebadf6d9e08e4ac209
Maxime Fournes <fournes.maxime@gmail.com>
2026-01-17 23:03:28 +0000

    fix: add dotenv package to load .env file automatically

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express, { Request, Response } from "express";
 import { createServer } from "http";
 import { GameState, AgentIdentity, AgentId } from "../shared/types.js";
